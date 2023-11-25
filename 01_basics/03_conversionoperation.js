68655d2f399f11129a5ec88adbb26bbd27896ceb
Jay Yadav <90307143+jaysgithubrepos@users.noreply.github.com>
2023-11-25 10:06:58 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,16 @@
+let score="33jaic";
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber= Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//number can be converted number
+//"33jaich" =>NaN
+//true=>1 false=>0
+//undefined =Nan
+let isLoggedIn=1;
+let booleanisLoggedIn=Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+//1 =>true 0=>false
