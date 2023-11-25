db5dd35deb3619f6824797256844ecd942046c88
Jay Yadav <90307143+jaysgithubrepos@users.noreply.github.com>
2023-11-25 08:59:47 +0000

    configuration commit

---
+++ 01_basics/01_basicsvariable.js
@@ -0,0 +1,7 @@
+const accountId=29;
+let accountName="Jaichand";
+let accountEmail="jayyadav010994@gmail.com";
+var accountPassword="Jaichand@29";
+accountCity="Pune";
+accountName="Reena";
+console.log(accountName);
