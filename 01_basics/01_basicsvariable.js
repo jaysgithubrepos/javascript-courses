c0c673cc27d6d0175fd65143c2671ad66062edbb
Jay Yadav <90307143+jaysgithubrepos@users.noreply.github.com>
2023-11-25 09:19:53 +0000

    variable commit

--- 01_basics/01_basicsvariable.js
+++ 01_basics/01_basicsvariable.js
@@ -2,6 +2,16 @@ const accountId=29;
 let accountName="Jaichand";
 let accountEmail="jayyadav010994@gmail.com";
 var accountPassword="Jaichand@29";
-accountCity="Pune";
+//accountCity="Pune"; not a good use 
+let accountCity= "Bengaluru";
+let accountBalance;
 accountName="Reena";
-console.log(accountName);
+//accountId=23;  const keyword cant be changed 
+//console.log(accountName);
+
+console.table([accountName,accountEmail,accountPassword,accountCity,accountBalance]);
+
+/* Prefer not to  use var
+ because of the issue of block scope and functional scope.
+
+*/
