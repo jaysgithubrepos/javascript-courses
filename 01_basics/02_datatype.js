007c894c8e4066dc2fc920a98a395d276890e886
Jay Yadav <90307143+jaysgithubrepos@users.noreply.github.com>
2023-11-25 09:39:09 +0000

    datatype commit

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+//alert("hello"); because we are using node js but not browser
+console.log("Jai"); console.log(4); //not a good way of writing code  ;code readabilty that should be high
+
+
+//documentation  https://tc39.es/ecma262/ -javascript Official website
+// Mdn web docs
+
+let name="Jai"; //String
+let age=29; //number 2 power of 53
+let isloggedIn=true;//boolean
+//bigint
+//null -is a standalone value
+//undefined 
+//undefined is value not declared to a variable
+//null is a special type 
+let state=null;
+console.table([name,age,isloggedIn,state]);
+//symbol =>unique
+//object is also datatype
+console.log(typeof(name));
+console.log(typeof(state));
+console.log(typeof(isloggedIn));
+
+
+
+
