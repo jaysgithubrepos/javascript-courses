359b643cc94c8b0b06e4c7a69bdf02d1bbe6f336
Jay Yadav <90307143+jaysgithubrepos@users.noreply.github.com>
2023-11-28 16:18:45 +0000

    String conversion

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,10 +1,10 @@
 let score="33jaic";
-console.log(typeof(score));
-console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof score);
 
-let valueInNumber= Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber= Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //number can be converted number
 //"33jaich" =>NaN
@@ -12,5 +12,32 @@ console.log(valueInNumber);
 //undefined =Nan
 let isLoggedIn=1;
 let booleanisLoggedIn=Boolean(isLoggedIn);
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 //1 =>true 0=>false
+let value=45;
+let negvalue= -value;
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(3/3);
+// console.log(3%3);
+
+var str="Hello";
+var str1="Jai";
+var str3=str +str1;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2+2);
+// console.log(1 + "");
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+
+
+let gamecounter=100;
+gamecounter ++;
+console.log(gamecounter);
