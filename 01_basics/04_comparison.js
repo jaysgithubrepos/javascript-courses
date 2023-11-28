8b3efa90a5ceb297e745f0cfdfbbf5eebb80f972
Jay Yadav <90307143+jaysgithubrepos@users.noreply.github.com>
2023-11-28 19:05:41 +0000

    comparison js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,6 @@
+// console.log(2>3); all these are easy operations when same data types are compared
+//the problem comes when the same data type are not compared
+console.log("2">1);
+console.log("02"< 1);//Try not to use comparison with other data type;
+
+
