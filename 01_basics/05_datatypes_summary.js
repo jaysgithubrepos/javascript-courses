f8264cee4302606fb2c2ffebc7a14410bc8dfe9c
Jay Yadav <90307143+jaysgithubrepos@users.noreply.github.com>
2023-11-28 19:27:04 +0000

    data type summary js

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,47 @@
+//Primitive data types
+//7 types
+//1.String
+//2.Number
+//3.Boolean
+//4.Null
+//5.undefined
+//6.symbol
+//7.BigInt
+
+//Non Primitive or Reference data types
+//1.Array
+//2.Objects
+//3.functions
+
+
+//Javascript is a single thread language
+
+const score=100;
+const scoreValue=100.3;
+const isloggedin=true;
+const temp=null;
+let userEmail;
+const id=Symbol('123');
+const  anotherid=Symbol('123');
+console.log(id==anotherid);
+
+const bigNumber=2134356567678768675643423523212332546546546572343546546546545675233245n;
+console.log(bigNumber);
+
+
+
+let heroes=["spiderman","batman","hanuman","shaktiman"];
+console.log(heroes);
+
+let myObject={
+    name:"jai",
+    age:29
+
+}
+console.log(myObject);
+
+let myperson=function jai(){
+console.log("hello");
+}
+
+//console.log(myperson());
